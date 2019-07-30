55490e40ec0ac79b369ae7f33f73da35f1be7910
Saranyatmc1996 <saranyatmc1996@gmail.com>
2019-07-30 11:33:06 +0530

    Second new code added on 30/7/2019

--- src/test/java/com/stepdefinition/TestRunner.java
+++ src/test/java/com/stepdefinition/TestRunner.java
@@ -12,3 +12,34 @@ import cucumber.api.junit.Cucumber;
 public class TestRunner {
 
 }
+/*
+package com.stepdefinition;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources", glue = "com.stepdefinition", plugin = { "html:target", "json:target/report.json","rerun:src/test/resources/failed.txt"},
+dryRun = false, monochrome = false, strict = true, tags="@regression")
+
+public class TestRunner {
+
+}
+*/
+/*
+package com.stepdefinition;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "@src/test/resources/failed.txt", glue = "com.stepdefinition",monochrome=true)
+
+public class TestReRunner {
+
+}
+*/
